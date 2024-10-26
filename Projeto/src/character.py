1303ceb53686d8c4bf3c9bb707d241011082ebca
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2024-10-26 11:15:50 -0300

    Criando o inicio das bases para as classes de personagem

--- Projeto/src/character.py
+++ Projeto/src/character.py
@@ -1,9 +1,18 @@
+from player_classes import *
+
 class Character:
-    def __init__(self, name:str, character_class:str, level:int):
+    def __init__(self, name:str, character_class:Character_Class_ABC, character_multiclass:list[Character_Class_ABC], level:int):
         self.name = name
         self.character_class = character_class
+        self.character_multiclass = character_multiclass
         self.level = level
+        
+    def get_character_level(self):
+        level = 0
+        
+        level += self.character_class.level
+        
+        for c in self.character_multiclass:
+            level += c.level
 
-    def level_up(self):
-        self.level += 1
-
+        self.level = level
