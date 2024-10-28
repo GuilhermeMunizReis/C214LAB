c85f017282fd414382314613097b46839ba0ac65
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2024-10-28 19:03:05 -0300

    Pequenos incrementos e mudanças nos testes de Character

--- Projeto/src/class_role.py
+++ Projeto/src/class_role.py
@@ -1,8 +1,12 @@
-class ClassRole:
-    def __init__(self, feats):
+from abc import ABC, abstractmethod
+
+class ClassRole(ABC):
+    def __init__(self, level: int, feats):
+        self.level = level
         self.life_die = 0
         self.feats = []
         
-
+    @abstractmethod
     def level_up(self):
         pass
+    
--- Projeto/test/characterTest.py
+++ Projeto/test/characterTest.py
@@ -1,4 +1,3 @@
-# characterTest.py
 import unittest
 import sys
 
@@ -8,16 +7,13 @@ from character import Character
 
 class TestCharacter(unittest.TestCase):
     def setUp(self):
-        self.char = Character("Eldrin", "Ranger", 5)
+        self.char = Character("Eldrin", "Ranger", [], 5)
 
     def testCharacterInstance(self):
         self.assertEqual(self.char.name, "Eldrin")
         self.assertEqual(self.char.character_class, "Ranger")
+        self.assertEqual(self.char.character_multiclass, [])
         self.assertEqual(self.char.level, 5)
-
-    def testLevelUp(self):
-        self.char.level_up()
-        self.assertEqual(self.char.level, 6)
 
 if __name__ == '__main__':
     unittest.main()
