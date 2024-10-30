75d7ac22a6a86f0effdca8de438968577572e35f
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2024-10-30 18:13:56 -0300

    Implementando a classe Character

--- Projeto/src/character.py
+++ Projeto/src/character.py
@@ -6,6 +6,24 @@ class Character:
         self.character_class = character_class
         self.character_multiclass = character_multiclass
         self.level = level
+        self.str_base = 1
+        self.dex_base = 1
+        self.con_base = 1
+        self.int_base = 1
+        self.wis_base = 1
+        self.cha_base = 1
+        self.str_value = 1
+        self.dex_value = 1
+        self.con_value = 1
+        self.int_value = 1
+        self.wis_value = 1
+        self.cha_value = 1
+        self.str_current = 1
+        self.dex_current = 1
+        self.con_current = 1
+        self.int_current = 1
+        self.wis_current = 1
+        self.cha_current = 1    
         
     def get_character_level(self):
         level = 0
