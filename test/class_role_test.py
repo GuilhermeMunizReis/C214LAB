d19a51bb64a97c57f26c882b42180d6ebf349272
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2024-11-13 18:25:24 -0300

    Desabilitando um teste inacabado

--- test/class_role_test.py
+++ test/class_role_test.py
@@ -5,16 +5,17 @@ from class_role import FighterClass
 
 class TestFighterClass(unittest.TestCase):
     def setUp(self):
-        self.character = Character(name="Astor", prof_bonus=0)
-        self.fighter = FighterClass(self.character, hp=20, level=0)
+        self.character = Character(name="Astor", race="Human", level=0)
+        self.fighter = FighterClass(self.character, hp=20, level=1)
     
+    """
     def test_level_up_initial_level(self):
         self.fighter.level_up(proficiencies="Athletics", feats="Archer")
         
         self.assertEqual(self.character.prof_bonus, 2)
         self.assertListEqual(self.character.proficiencies, ["Athletics", "Intimidation"])
         self.assertListEqual(self.fighter.feats, ["Fighting style - Archer", "Retake Breath"])
-
+    """
 
 if __name__ == "__main__":
     unittest.main()
