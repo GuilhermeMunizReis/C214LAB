0bac79b0e31e12ddb9c9a566709e0adb16dce683
Raissa Carlucio <rabe.brasil@gmail.com>
2024-11-21 15:52:25 -0300

    Atualizando a classe currency e adicionando testes em currency_test

--- src/currency.py
+++ src/currency.py
@@ -6,3 +6,39 @@ class Currency:
 
     def set_value(self, new_value:float):
         self.value = new_value
+    
+    #Adiciona um valor à moeda.
+    def add_value(self, amount: float):
+        self.value += amount
+
+    # Subtrai um valor da moeda, se houver saldo suficiente.Se não houver, o saldo continua igual.
+    def subtract_value(self, amount: float):
+        if self.value - amount >= 0:
+            self.value -= amount
+        else:
+            print("Valor insuficiente.")
+    
+    #Retorna uma string representando a moeda, incluindo seu nome, abreviação e valor.
+    def __str__(self):
+        return f"{self.name} ({self.shortname}): {self.value}"
+    
+
+if __name__ == "__main__":
+    # Criando uma instância de moeda
+    gold = Currency("Gold", "GP", 100.0)
+    
+    print(gold)  
+
+    gold.set_value(150.0)
+    print(gold)  
+    
+
+    gold.add_value(50.0)
+    print(gold)  
+    
+
+    gold.subtract_value(75.0)
+    print(gold)  
+    
+
+    gold.subtract_value(200.0)  
--- test/currency_test.py
+++ test/currency_test.py
@@ -19,6 +19,42 @@ class TestCurrency(unittest.TestCase):
         self.assertEqual(c.name, "Ardin")
         self.assertEqual(c.shortname, "ard")
         self.assertNotEqual(c.value, 20)
+        
+    # Testando a alteração do valor da moeda
+    def testSetValue(self):
+        c = Currency("Gold", "GP", 10.0)
+        c.set_value(20.0)
+
+        self.assertEqual(c.value, 20.0)
+        
+
+    # Testando a adição de valor à moeda
+    def testAddValue(self):
+        c = Currency("Gold", "GP", 50.0)
+        c.add_value(25.0)
+
+        self.assertEqual(c.value, 75.0)   
+
+    # Testando a subtração de valor
+    def testSubtractValue(self):
+        c = Currency("Gold", "GP", 100.0)
+        c.subtract_value(30.0)
+
+        self.assertEqual(c.value, 70.0)
+        
+    # Testando a subtração de valor com saldo insuficiente
+    def testSubtractValueInsufficient(self):
+        c = Currency("Gold", "GP", 50.0)
+        c.subtract_value(100.0)  
+
+        self.assertEqual(c.value, 50.0)  # O valor não deve ser alterado
+
+   # Testando a subtração de valor que deixa a moeda com saldo 0
+    def testSubtractValueZeroBalance(self):
+        c = Currency("Gold", "GP", 50.0)
+        c.subtract_value(50.0)
+
+        self.assertEqual(c.value, 0.0) 
 
 if __name__ == '__main__':  
     unittest.main()
