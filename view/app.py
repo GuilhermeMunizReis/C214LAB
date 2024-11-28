b245606e8843728a39f48f3ead4a39df095ae14d
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2024-11-28 15:19:57 -0300

    Pequenas alterações

--- view/app.py
+++ view/app.py
@@ -10,19 +10,37 @@ class App:
     def __init__(self):
         self.app = Game(resolution=(1200,700))
         self.shop = get_shop_std()
+        self.screen = "market"              #Screen will be stored here
      
     def start(self):
         self.set_screen()
         self.app.start()
 
     def set_screen(self):
+
+        if self.screen == "menu":
+            pass
+        if self.screen == "market":
+            self.market_screen()
+    
+    def market_screen(self):
         screen = Screen()
 
-        name_lb = Label(text=self.shop.name, x=50, y=50, height=30, width=200)
+        name_lb = Label(text=self.shop.name, x=50, y=30, height=30, width=200)
         screen.add(name_lb)
 
-        owner_lb = Label(text=self.shop.owner, x=300, y=50, height=30, width=200)
+        owner_lb = Label(text=self.shop.owner, x=300, y=30, height=30, width=200)
         screen.add(owner_lb)
+
+        name_item = Label(text="Item", x=50, y=90, height=30, width=200)
+        screen.add(name_item)
+
+        dmg_item = Label(text="Description", x=270, y=90, height=30, width=400)
+        screen.add(dmg_item)
+
+        price_item = Label(text="Price", x=690, y=90, height=30, width=200)
+        screen.add(price_item)
+
 
         cont = 1
         for item in self.shop.itens:
